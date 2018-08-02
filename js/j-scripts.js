90273678ed13fb54ed255021652cc82856ead3ea
drevitsky <4227723@gmail.com>
2018-08-02 14:13:12 +0300

    first commit

---
+++ js/j-scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+$(document).ready(function(){
+
+$('.slider').slick({
+	//dots:true,
+	slidesToShow: 4,
+	//centerPadding:'180px'
+	//infinite: false,
+	
+	//easing:'swing'
+});
+
+
+});
+//єффект fade
